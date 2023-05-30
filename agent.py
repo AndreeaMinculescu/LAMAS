2c365dd2f2f8f32b75370c48d500b7b54f5d23d3
imruhi <61055561+imruhi@users.noreply.github.com>
2023-05-30 16:44:05 +0200

    added greedy strategy for agents

--- agent.py
+++ agent.py
@@ -1,3 +1,5 @@
+import numpy as np
+import random
 class Agent:
   
     def __init__(self, cards, table):
@@ -6,29 +8,68 @@ class Agent:
         # table is a list of 6 card objects on the table
         self.table = table
 
-    def greedy_strategy():
-        """
-        Find the most common number in all the cards visible to 
-        the agent (their cards and the cards on table) and then 
-        pick up the same number from the pile on the table otherwise
-        don't do anything. If there are multiple common numbers take the 
-        first one.
-        """
+def greedy_strategy():
+    """ 
+    Find all common numbers (occurence more than once) in all the cards visible 
+    to the agent (their cards and the cards on table) and then pick up the same 
+    number from the pile on the table otherwise don't do anything. While 
+    swapping, make sure you do not swap out any possible wanted cards. If there 
+    are multiple common numbers to discard from agent's hand then choose a random 
+    one to discard.
+    """
 
-        card_values = [i.value for i in self.cards]
-        table_values = [i.vale for i in self.table]
+    # TODO: adjust to andreea's swap code and agent env 
 
-        all_values = card_values + table_values
+    card_values = [1,2,3,4]
+    table_values = [8,6,12,12,11,6]
 
-        # most frequent number visible to you
-        most_freq = max(set(all_values), key=all_values.count)
+    all_values = card_values + table_values
 
-        for card in self.table:
-            if most_freq == card.value:
+    possible_wants_1 = sorted([value for value in all_values if all_values.count(value) > 1], reverse= True)
+    possible_wants = []
 
-                # do swap 
+    # first give priority to the wants which are in starting hand
+    for want in possible_wants_1:
+        if want in card_values:
+            possible_wants.append(want)
 
+    possible_wants.extend([i for i in possible_wants_1 if i not in possible_wants])
 
+    print("Wanted cards: ", possible_wants)
+    print("Cards on table: ", table_values)
+    print("Cards in hand: ", card_values)
+    print("------------------------------------")
 
+    for want in possible_wants:
+        print("Current wanted card: ", want)
+        
+        if want in table_values:
 
+            discards = [value for value in card_values if card_values.count(value) == 1 and value not in possible_wants]
 
+            if want in card_values:
+                discards = [value for value in card_values if value != want and value not in possible_wants]
+
+            if discards:
+                swap = random.choice(discards)
+                # the card number to swap is the var swap
+                # the card number wanted is the var want
+                idx1 = card_values.index(swap)
+                idx2 = table_values.index(want)
+
+                card_values[idx1] = want
+                table_values[idx2] = swap
+
+            else:
+                print("Nothing to discard, current hand has wanted cards")
+                return
+
+        else:
+            print(f"Skip {want} since not on table")
+
+        print("Cards on table: ", table_values)
+        print("Cards in hand: ", card_values)
+        print()
+
+# test
+greedy_strategy()
